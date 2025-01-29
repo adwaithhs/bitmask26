e28ee779e907654192151f960abdac072f40cee3
Adwaith <adwaithhs@gmail.com>
2025-01-30 02:33:18 +0530

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="bitmask26",
-    version="0.1.0",
+    version="0.1.1",
     author="Adwaith H Sivam",
     author_email="adwaithhs@gmail.com",
     description="A C++ optimized bitmask implementation with Python bindings",
